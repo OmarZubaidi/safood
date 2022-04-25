0aaee34d75098f6effa6abfb2e65e0a1310dc601
Gabriele Zannini <gabri.zanna@gmail.com>
2022-04-25 16:04:33 +0200

    moving navbar logic into app page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,7 @@
-import React from "react"
+import React, { useRef, useState,useEffect } from 'react'
 import Signup from './Authentication/Signup'
 import "bootstrap/dist/css/bootstrap.min.css"
-import {Container} from "react-bootstrap"
+import {Container, Navbar, Nav, Form, Button} from "react-bootstrap"
 import { AuthProvider } from '../context/AuthContext'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Profile from './Profile'
@@ -11,11 +11,54 @@ import MainPage from './MainPage'
 import Events from './Events'
 import { QueryClientProvider, QueryClient } from "react-query"
 
+import Dashboard from './Dashboard'
+import RecipeContainer from './RecipeContainer';
+import { useAuth } from '../context/AuthContext';
+
+import {getUser, recipeQuery } from './service';
+import {useQuery, useMutation, useQueryClient} from "react-query"
+
 const queryClient = new QueryClient()
 
 function App() {
 
     
+  const target = useRef(null);
+  const stringRef = useRef('');
+  const {currentUser} = useAuth();
+  const [search, setSearch] = useState(false)
+  const [recipes, setRecipes] = useState()
+
+  async function fetchUser(){
+    const res = await getUser(currentUser)
+    return res.json()
+  }
+
+  const {data: profile, status} = useQuery("user", fetchUser)
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setSearch(true)
+    try {
+      recipeQuery(profile.allergens, stringRef.current.value)
+      .then(response => response.json())
+      .then(data => {setRecipes(data)}
+      );
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+  if(status === "loading"){
+    return <div>loading</div>
+  }
+
+  if(status === "error"){
+    return <div>error</div>
+  }
+
+
     return (
         
 
@@ -23,17 +66,32 @@ function App() {
         className='d-flex align-items-center justify-content-center ' 
         style={{minHeight: "100vh"}}>
             <div className='w-100' style={{minWidth: "400px"}}>
+            <Navbar  fixed="top" expand="sm" bg='success' variant="dark" style={{height: '200px' }} /*   */ >
+                <Container>
+                    <Navbar.Brand href='/'>
+                        Safood
+                    </Navbar.Brand>
+                    <Form /* onSubmit={handleSubmit} */ className='d-flex'>
+                        <Form.Control type="text" placeholder='Search a recipe!' /* ref={stringRef} */>
+                        </Form.Control>
+                        <Button type='submit' > Search!</Button> 
+                    </Form>
+                    <Nav>
+                        <Nav.Link href='/profile'> Profile </Nav.Link>
+                        <Nav.Link href='/'> Notifications </Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
                 <Router>
                 <QueryClientProvider client={queryClient}>
-
                 <AuthProvider>
                     <Routes>
-                        <Route exact path="/" element={<PrivateRoute/>}></Route>
                         <Route path="/main" element={<MainPage/>} />
                         <Route path="/signup" element={<Signup/>} />
                         <Route path="/login" element={<Login/>} />
                         <Route path="/profile" element={<Profile/>} />
                         <Route path="/events" element={<Events/>} />
+                        <Route exact path="/" element={<PrivateRoute/>}></Route>
 
                     </Routes>
                 </AuthProvider>
