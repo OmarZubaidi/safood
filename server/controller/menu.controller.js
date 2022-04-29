222c727b99ecb563b18e977fc449578f1ab51631
Omar Zubaidi <66521244+OmarZubaidi@users.noreply.github.com>
2022-04-29 20:51:10 +0100

    Refactor controller functions

--- server/controller/menu.controller.js
+++ server/controller/menu.controller.js
@@ -1,22 +1,21 @@
 // Local imports
 const db = require('../db');
+const recipeHasNoAllergens = require('../utils/recipeHasNoAllergens');
 const sample = require('../utils/sampleFromArray');
 
 function getMenu (req, res) {
   const allergens = JSON.parse(req.headers.allergens);
   const courses = ['starter', 'main', 'side'];
   const menu = [];
+
   courses.forEach(course => {
-    const recipe = db.filter(recipe => {
-      return !allergens.some(allergen =>
-        recipe
-          .ingredients
-          ?.includes(allergen.toLowerCase())
-        && recipe.dishTypes?.includes(course)
-      );
-    });
+    const recipe = db.filter(recipe =>
+      recipeHasNoAllergens(recipe, allergens)
+      && recipe.dishTypes?.includes(course)
+    );
     menu.push(...sample(recipe, 1));
   });
+
   res.send(menu);
 }
 
