51a1187e898f7115a2953b3648d78f84c18ef349
Nicholas Allen <nicallennn@gmail.com>
2022-04-30 09:37:06 +0100

    implmeneted signup test

--- client/src/components/tests/Signup.test.jsx
+++ client/src/components/tests/Signup.test.jsx
@@ -3,40 +3,58 @@ import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 
 // Local imports
-import Login from '../Authentication/Login';
+import Signup from '../Authentication/Signup';
+
+const mockLogin = {
+  name: "nick",
+  email: "n@1.com",
+  password: '123456'
+
+};
 
 jest.mock('../../context/AuthContext.js', () => ({
   useAuth: () => {
     return {
-      login: (mockLogin) => {
-        // console.log(mockLogin);
+      signup: (email, password ) => {
+      return {
+        user: {
+          uid: '12345'
+        }
       }
     }
   } 
+  }
 }));
 
-describe('Login component', () => {
+jest.mock('../../services', () => ({
+  postUser: (user) => {
+    console.log(user);
+  } 
+}));
+
+describe('Signup component', () => {
 
   test('should match the snapshot', () => {
     const { container } = render(
       <BrowserRouter>
-        <Login />
+        <Signup />
       </BrowserRouter>
     );
     expect(container).toMatchSnapshot();
   });
 
-  test('should render the login component', () => {
+  test('should render the signup component', () => {
     render(
       <BrowserRouter>
-        <Login />
+        <Signup />
       </BrowserRouter>
     );
+    screen.getByText('Name');
     screen.getByText('E-mail');
     screen.getByText('Password');
+    screen.getByText('Confirm password');
+    screen.getByText('Tell us something about yourself');
     screen.getByRole('button');
   });
 
-
 });
-
