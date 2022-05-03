2cc18f52e82f86fb58733eb4d4e63410e73a0c8d
Omar Zubaidi <66521244+OmarZubaidi@users.noreply.github.com>
2022-05-03 15:57:54 +0100

    Add dashboard tests

--- client/cypress/integration/02 - Dashboard.spec.js
+++ client/cypress/integration/02 - Dashboard.spec.js
@@ -4,5 +4,24 @@ describe('Dashboard tests', () => {
 
     cy.url()
       .should('include', '/main');
+
+    for (let i = 0; i < 4; i++) {
+      cy.get(`#recipe-${i}`)
+        .should('contain', 'Prep-time');
+    }
+  });
+
+  it('should render the other user(s)', () => {
+    cy.viewport(1000, 2000);
+
+    cy.get('#users-container')
+      .children()
+      .should('exist');
+    cy.get('#users-container > .col > .card > .card-title');
+    // Assuming bye@bye.com exists
+    cy.get('#users-container > .col > .card > .card-title')
+      .should('contain', 'About me');
+    cy.get('#users-container > .col > .card > .card-body')
+      .should('contain', 'bye');
   });
 });
